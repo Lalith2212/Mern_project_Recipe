5ef1c49614630ff53f8f31f7f34ca702a7ba3cc8
lalith krishna <lalithkrishna2212@gmail.com>
2025-03-17 15:28:42 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ const PORT=process.env.PORT || 3000
 connectDb()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://mern-project-recipe.vercel.app', // Allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
+    credentials: true // Allow cookies and authorization headers if needed
+}));
+
 app.use(express.static("public"))
 
 app.use("/",require("./routes/user"))
